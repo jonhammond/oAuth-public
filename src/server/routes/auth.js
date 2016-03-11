fc35a94db18bd46f89d8e055ece38a8c0fec600e
Jon Hammond <jonh1016@gmail.com>
2016-03-11 10:10:54 -0700

    Set up passport in app.js, and created auth.js, set up routes for app.js, and added .env

---
+++ src/server/routes/auth.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/linkedin', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.get('/linkedin/callback', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.redirect('/');
+});
+
+module.exports = router;
